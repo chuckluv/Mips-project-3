3f17bb0e98ce7519b1f643f539a067e7e34ed99e
chuckluv <calovett35@gmail.com>
2019-11-23 21:55:56 -0500

    fixes some errors so now it stores the input into an array and prints everything after the comma

--- project3.s
+++ project3.s
@@ -9,13 +9,17 @@ main :
 	la $a0,data
 	li $a1, 1001
 	syscall
+	la $t0, data
+load:
+	
+	la $t1, storage
 
 loop:
-	li $t0, data
-	li $t1, storage
+	
 	
 	lb $s0, ($t0)
-	beq $s0, 44, print
+	beq $s0, 0, print
+	beq $s0, 10, print
 	sb $s0, ($t1)
 
 	addi $t0,$t0,1
@@ -23,13 +27,16 @@ loop:
 	j loop
 	
 print:
+	la $t0,storage
+	addi $t0, $t0, 3
 	li $v0, 4
 	la $a0, output
 	syscall
 
 	li $v0, 4
 	
-	move $a0, $t1
+	move $a0, $t0
 	syscall
 	
-	
+	li $v0, 10
+	syscall
